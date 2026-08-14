bcfb28d8e5d832f10983686bfe81da28aa345000
prcdevgitbot <webdev@pewresearch.org>
2026-08-14 02:36:46 +0000

    See https://github.com/pewresearch/prc-platform/commit/40599fdca9812eb1361793eaab80c311bfc7b02e from refs/tags/v1.13.1

--- includes/class-rest-api.php
+++ includes/class-rest-api.php
@@ -1262,6 +1262,7 @@ class REST_API {
 			'type'             => $type,
 			'title'            => get_the_title( $post ),
 			'status'           => $post->post_status,
+			'previousStatus'   => (string) get_post_meta( $post->ID, '_wp_trash_meta_status', true ),
 			'date'             => mysql2date( 'c', $post->post_date, false ),
 			'modified'         => mysql2date( 'c', $post->post_modified, false ),
 			'edit_url'         => get_edit_post_link( $post->ID, 'raw' ),
