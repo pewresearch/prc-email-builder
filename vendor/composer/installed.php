17473880060406b37dfe83577d9bee4a84d649bf
prcdevgitbot <webdev@pewresearch.org>
2026-09-04 04:21:44 +0000

    See https://github.com/pewresearch/prc-platform/commit/99299fc9b3d1ae208ec034696957dff31611d4cb from refs/heads/trunk

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'prc/email-builder',
         'pretty_version' => 'dev-trunk',
         'version' => 'dev-trunk',
-        'reference' => '804ddde449658d8cb94e2325ff2a2de1d02e8bbf',
+        'reference' => '99299fc9b3d1ae208ec034696957dff31611d4cb',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -58,7 +58,7 @@
         'prc/email-builder' => array(
             'pretty_version' => 'dev-trunk',
             'version' => 'dev-trunk',
-            'reference' => '804ddde449658d8cb94e2325ff2a2de1d02e8bbf',
+            'reference' => '99299fc9b3d1ae208ec034696957dff31611d4cb',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
